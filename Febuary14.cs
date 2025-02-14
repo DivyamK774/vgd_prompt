2416a775c455ab2470cebec677f2c74c4e2726de
DivyamK774 <DivyamK190@stemcollegiate.ca>
2025-02-14 10:08:07 -0700

    Create Febuary14.cs

---
+++ Febuary14.cs
@@ -0,0 +1,9 @@
+//Journal prompt: Febuary 14, 2025
+string firstName;
+char userOption;
+int gameScore;
+decimal particlesPerMillion;
+bool processedCustomer;
+//I learned that I can name different variables, but I must be careful with what I name 
+//them. This is because if I name the variable something that already exists, like integer, 
+//it would not work as an variablea dn instead would work as an integer.
