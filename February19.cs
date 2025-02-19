8d0dcde9aaf32da0ab751a320b46587907ff0c1b
DivyamK774 <DivyamK190@stemcollegiate.ca>
2025-02-19 08:44:22 -0700

    Create February19.cs

---
+++ February19.cs
@@ -0,0 +1,14 @@
+//Journal prompt: February 19, 2025
+string firstName;
+firstName = "Bob";
+Console.WriteLine(firstName);
+firstName = "Liem";
+Console.WriteLine(firstName);
+firstName = "Isabella";
+Console.WriteLine(firstName);
+firstName = "Yasmin";
+Console.WriteLine(firstName);
+//I learned that I have to set the variable, before I retrive a value from the variable. 
+//I also learned that to declare a variable, I have to use an equal sign. I can change 
+//the variable , so that every time i do the writeline comand, it will change the name
+//I see when I run the code.
