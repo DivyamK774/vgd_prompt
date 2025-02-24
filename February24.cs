317c1800ddf4e5000b23f20fc3e1eb07a53391d6
DivyamK774 <DivyamK190@stemcollegiate.ca>
2025-02-24 14:09:22 -0700

    Create February24.cs

---
+++ February24.cs
@@ -0,0 +1,14 @@
+//Journal prompt: February 24, 2025
+var name = "Bob!";
+var message = "3";
+var temp = "34.4";
+Console.Write("Hello, ");
+Console.Write(name);
+Console.Write(" You have ");
+Console.Write(message);
+Console.Write(" messages in your inbox. The temperature is ");
+Console.Write(temp);
+Console.Write(" celsius.");
+//I learned that the var keyword tells that the data type is implied by the assigned value. 
+//You have to use the console.write code to run the message. I will also see the var keyword-
+//in many other peoples codes butI should use the data type when possible. 
