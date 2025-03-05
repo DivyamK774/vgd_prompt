138ed5ba196228c01bcf378a745fa993632fc014
DivyamK774 <DivyamK190@stemcollegiate.ca>
2025-03-05 09:28:20 -0700

    Create March5.cs

---
+++ March5.cs
@@ -0,0 +1,8 @@
+//Journal prompt: March 5, 2025
+Console.WriteLine("123");
+Console.WriteLine(123);
+Console.WriteLine("true");
+Console.WriteLine(true);
+// I learned that there are many data types in c#, like string, char, int, and more. String is used for words and phrases,
+// char is used for single letters, int is used for whole numbers, decimal for fractional components, and bool for true or
+// false components.
