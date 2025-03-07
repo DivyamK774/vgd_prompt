4c80cd9e0f093e467333e3710f9ec4a676f53c20
DivyamK774 <DivyamK190@stemcollegiate.ca>
2025-03-07 10:29:30 -0700

    Create March7.cs

---
+++ March7.cs
@@ -0,0 +1,27 @@
+//Journal prompt: March 7, 2025
+//Unit 3/4:
+string firstName;
+firstName = "Bob";
+Console.WriteLine(firstName);
+firstName = "Liem";
+Console.WriteLine(firstName);
+firstName = "Isabella";
+Console.WriteLine(firstName);
+firstName = "Yasmin";
+Console.WriteLine(firstName);
+//Unit 5/6/7
+string name = "Bob";
+int msg = 3;
+decimal temp = 34.4m;
+Console.Write("Hello, ");
+Console.Write(name);
+Console.Write(" ! You have ");
+Console.Write(msg);
+Console.Write(" messages in your inbox. The temperature is ");
+Console.Write(temp);
+Console.Write(" celsius.");
+// I learnt that you must set or assign a variable before retriving it, and you need to intalize the variable by 
+// assigning a value before declaration. Another thing I learnt was that the var keyword tells the complier to deduce 
+// the data type based on the valur it is assigned. There are many types of variables like string thats use for phrases, 
+// char which is used for single letters, into is used for whole numbers, decimal for fractional components, and finaly, 
+// bool for true and false components.
