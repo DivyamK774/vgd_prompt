5c64f4bec53ba370b5f607842259835e6e2cf1a8
DivyamK774 <DivyamK190@stemcollegiate.ca>
2025-03-17 14:28:32 -0600

    Create March17.cs

---
+++ March17.cs
@@ -0,0 +1,10 @@
+//Journal prompt: March 17, 2025
+string projectName = $"ACME";
+string projectOutput = $@"c:\Exercise\{projectName}\data.txt";
+Console.WriteLine( $"View English output:\n\t{projectOutput}");
+string russianMessage = $"\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+string russianOutput = $@" c:\Exercise\ACME\ru-RU\data.txt";
+Console.WriteLine( $"\n{russianMessage}\n{russianOutput}");
+// Today, we did a challenge on using variables, special characters and output code to make a message.
+// It helped me realise the uses af many of the different variariables and special characters by 
+// actually making the code ourselves. Unit 7 quized me on the entire topic, and I got a 4/4 on it.
