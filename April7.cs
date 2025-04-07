b5e838ad23e082d3c06bce37c79b36e854800505
DivyamK774 <DivyamK190@stemcollegiate.ca>
2025-04-07 13:49:02 -0600

    Create April7.cs

---
+++ April7.cs
@@ -0,0 +1,7 @@
+//Journal prompt: April 7, 2025: Module 5, Unit 1+2
+string firstName = "Bob";
+int widgetsSold = 7;
+Console.WriteLine(firstName + " sold " + (widgetsSold + 7) + " widgets.");
+// Today, I learned that you can perform math using C-sharp. You can use the "+" symbol to add,
+// and also do string concatenation. The C# complier will convert and "int" code into a "string".
+// You can finally use brackets to define the order of opperations.
