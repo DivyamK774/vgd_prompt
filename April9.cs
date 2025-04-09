90fcc164480a0181b58fa6576637489d3b9b99ca
DivyamK774 <DivyamK190@stemcollegiate.ca>
2025-04-09 09:00:57 -0600

    Create April9.cs

---
+++ April9.cs
@@ -0,0 +1,12 @@
+//Journal prompt: April 7, 2025: Module 5, Unit 3+4
+int value = 1;
+value++;
+Console.WriteLine("First: " + value);
+Console.WriteLine($"Second: {value++}");
+Console.WriteLine("Third: " + value);
+Console.WriteLine("Fourth: " + (++value));
+// Today, I learned that I can use *, /, +, and - for mathematical perposes. I can also use % to get the remainder
+// after the division. The operations will be done in the order of BEDMAS. I can use compound opperators like +=, 
+// -=, *=, ++, and -- to increment or decrement and then assign the result into the original variable. Finally, I 
+// learned incrementing and decrement perform differently depending on in the operator is before or after the 
+// operand.
