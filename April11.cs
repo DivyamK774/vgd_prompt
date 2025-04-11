0f27bae9de73d192754a52627f0b51404dc5c4f5
DivyamK774 <DivyamK190@stemcollegiate.ca>
2025-04-11 10:10:18 -0600

    Create April11.cs

---
+++ April11.cs
@@ -0,0 +1,7 @@
+//Journal prompt: April 11, 2025: Module 5, Unit 5+6
+int fahrenheit = 94;
+decimal celsius = (fahrenheit - 32) * (5m / 9m);
+Console.WriteLine($"The temperature is {celsius} Celsius.");
+// Today, I learned how to convert Fahrenheit to Celsius using code. I can 
+// easilly use math for many persouses in code, like using it to convert 
+// between two different scales.
