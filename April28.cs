3921ce39d03b19e8611dcd5bdf9865f08905d1ba
DivyamK774 <DivyamK190@stemcollegiate.ca>
2025-04-30 08:30:13 -0600

    Create April28.cs

---
+++ April28.cs
@@ -0,0 +1,33 @@
+//Journal prompt: April 28, 2025: Module 6, Unit 1-3
+int currentAssignments = 5;
+int sophia1 = 93;
+int sophia2 = 87;
+int sophia3 = 98;
+int sophia4 = 95;
+int sophia5 = 100;
+int nicolas1 = 80;
+int nicolas2 = 83;
+int nicolas3 = 82;
+int nicolas4 = 88;
+int nicolas5 = 85;
+int zahirah1 = 84;
+int zahirah2 = 96;
+int zahirah3 = 73;
+int zahirah4 = 85;
+int zahirah5 = 79;
+int jeong1 = 90;
+int jeong2 = 92;
+int jeong3 = 98;
+int jeong4 = 100;
+int jeong5 = 97;
+int sophiaSum = sophia1 + sophia2 + sophia3 + sophia4 + sophia5;
+int nicolasSum = nicolas1 + nicolas2 + nicolas3 + nicolas4 + nicolas5;
+int zahirahSum = zahirah1 + zahirah2 + zahirah3 + zahirah4 + zahirah5;
+int jeongSum = jeong1 + jeong2 + jeong3 + jeong4 + jeong5;
+Console.WriteLine("Sophia: " + sophiaSum / currentAssignments);
+Console.WriteLine("Nicolas: " + nicolasSum / currentAssignments);
+Console.WriteLine("Zahirah: " + zahirahSum / currentAssignments);
+Console.WriteLine("Jeong: " + jeongSum / currentAssignments);
+// Today, I learned how to create a calculator using C# code to calculate 
+// the average grade of each student. I also changed the end of the code
+// so that it divides the total points bt the number of assignments.
